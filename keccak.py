aed02c7eac815542bc6531ce39b7859757827747
Abdul-Karim Farhat <ak.f@live.com>
2014-12-07 09:57:21 -0500

    Added mapping functions and first step (theta)

--- keccak.py
+++ keccak.py
@@ -14,17 +14,44 @@ class Keccak:
             self.l = int(math.log(self.w, 2))
 
     def string_to_array(self, string):
+
+        a = [[[string[self.w * (5 * y + x) + z] for z in range(self.w)] 
+                                                for y in range(5)]
+                                                for x in range(5)]
+        '''
         a = [][][]
         
         for x in range(5):
             for y in range(5):
                 for z in range(w):
                     a[x][y][z] = string[w*(5*y+x)+z]
+        '''
 
         return a
 
     def array_to_string(self, array):
-        s = ''
+        lanes = [[''.join([array[i][j][x] for x in range(self.w)])
+                                          for j in range(5)]
+                                          for i in range(5)]
 
+        planes = [''.join([lanes[i][j] for i in range(5)])
+                                       for j in range(5)]
 
+        s = ''.join([planes[i] for i in range(5)])
+        
+        return s
+
+    def step_theta(self, state_array):
+        C = [[reduce(lambda i, j: i^j, [state_array[x][y][z] for y in range(5)])
+                                                             for x in range(5)]
+                                                             for z in range(self.w)]
+
+        D = [[C[(x-1) % 5][z] ^ C[(x+1) % 5][(z-1) % self.w] for x in range(5)]
+                                                             for z in range(self.w)]
+
+        A = [[[state_array[x][y][z] ^ D[x][z] for x in range(5)]
+                                              for y in range(5)]
+                                              for z in range(self.w)]
+
+        return A
 
